95952dbaf8cbcfc6b590ed2f51864eab18f6acd6
Daniil Krasnov <dk98126@yandex.ru>
2019-06-16 11:47:17 +0300

    echo messages and send info about biggest sent photo

--- src/main/java/ShoppingListBot.java
+++ src/main/java/ShoppingListBot.java
@@ -1,7 +1,12 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ShoppingListBot extends TelegramLongPollingBot {
     public String getBotToken() {
@@ -12,9 +17,41 @@ public class ShoppingListBot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
                     .setChatId(update.getMessage().getChatId())
-                    .setText("Я знаю, где ты находишься:\n" + update.getMessage().getLocation().toString());
+                    .setText("Поторяю за тобой:\n" + update.getMessage().getText());
             try {
                 execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        } else if (update.hasMessage() && update.getMessage().hasPhoto()) {
+            long chat_id = update.getMessage().getChatId();
+
+            List<PhotoSize> photoSizes = update.getMessage().getPhoto();
+
+            String f_id = photoSizes.stream()
+                            .sorted(Comparator.comparing(PhotoSize::getFileSize)
+                            .reversed())
+                            .findFirst()
+                            .orElse(null)
+                            .getFileId();
+
+            int f_width = photoSizes.stream()
+                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+                    .findFirst()
+                    .orElse(null).getWidth();
+
+            int f_height = photoSizes.stream()
+                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+                    .findFirst()
+                    .orElse(null).getHeight();
+
+            String caption = "file_id: " + f_id + "\nwidth: " + f_width + "\nheight: " + f_height;
+            SendPhoto msg = new SendPhoto().setChatId(chat_id)
+                                .setChatId(chat_id)
+                                .setPhoto(f_id)
+                                .setCaption(caption);
+            try {
+                execute(msg);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
