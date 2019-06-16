00139667addfca5747e1e9a45cb38b728d8e4b77
Daniil Krasnov <dk98126@yandex.ru>
2019-06-16 15:51:57 +0300

    urban dictionary basic integration

--- src/main/java/ShoppingListBot.java
+++ src/main/java/ShoppingListBot.java
@@ -1,3 +1,7 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
@@ -5,7 +9,9 @@ import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.IOException;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 
 public class ShoppingListBot extends TelegramLongPollingBot {
@@ -17,25 +23,8 @@ public class ShoppingListBot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
-            if (message_text.equals("/start")) {
-                SendMessage message = new SendMessage().setChatId(chat_id).setText(message_text);
-                try {
-                    execute(message); // Call method to send the message
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            } else if (message_text.equals("/pic")) {
-                SendPhoto message = new SendPhoto().setChatId(chat_id)
-                        .setPhoto("AgADAgAD36oxG52vMUheQSgTB0JSf5RrUw8ABOLV4pVOGRXy64gEAAEC")
-                        .setCaption("Photo");
-                try {
-                    execute(message);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                SendMessage message = new SendMessage()
-                        .setChatId(chat_id).setText("Unknown command");
+            if (message_text.equals("/udword")) {
+                SendMessage message = new SendMessage().setChatId(chat_id).setText(lastWordFromUrbanDictionaryDescription());
                 try {
                     execute(message);
                 } catch (TelegramApiException e) {
@@ -47,5 +36,46 @@ public class ShoppingListBot extends TelegramLongPollingBot {
 
     public String getBotUsername() {
         return "ShoppingListBot";
+    }
+
+    public String lastWordFromUrbanDictionaryDescription() {
+        StringBuilder builder = new StringBuilder();
+        Document doc = null;
+        String dateString;
+        String url = "https://www.urbandictionary.com/?page=1";
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (doc != null) {
+            Elements defPanels = doc.getElementsByClass("def-panel");
+            Iterator<Element> defPanelsIterator = defPanels.iterator();
+            if (defPanelsIterator.hasNext()) {
+                Element defPanel = defPanelsIterator.next();
+                Elements dates = defPanel.getElementsByClass("ribbon");
+                Iterator<Element> datesIterator = dates.iterator();
+                if (datesIterator.hasNext()) {
+                    dateString = datesIterator.next().text();
+                    builder.append("<i>").append(dateString).append("</i>").append("\n");
+                }
+                Elements words = defPanel.getElementsByClass("word");
+                Iterator<Element> wordsIterator = words.iterator();
+                if (wordsIterator.hasNext()) {
+                    Element word = wordsIterator.next();
+                    builder.append("<b>").append(word.text()).append("</b>").append("\n");
+                }
+                Elements meanings = defPanel.getElementsByClass("meaning");
+                Iterator<Element> meaningsIterator = meanings.iterator();
+                if (meaningsIterator.hasNext()) {
+                    Element meaning = meaningsIterator.next();
+                    String meaningString = meaning.html().replaceAll("<br>\n", "\n");
+                    meaningString = meaningString.replaceAll("\n\n", "\n");
+                    builder.append(meaningString);
+                }
+            }
+        }
+        return builder.toString();
     }
 }
