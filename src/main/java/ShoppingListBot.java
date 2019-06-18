97c812696a36225ef40d38e933950b0347e5af81
Daniil Krasnov <dk98126@yandex.ru>
2019-06-18 23:39:12 +0300

    random word feature

--- src/main/java/ShoppingListBot.java
+++ src/main/java/ShoppingListBot.java
@@ -4,15 +4,14 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
-import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
-import java.util.Comparator;
 import java.util.Iterator;
-import java.util.List;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ShoppingListBot extends TelegramLongPollingBot {
     public String getBotToken() {
@@ -24,7 +23,7 @@ public class ShoppingListBot extends TelegramLongPollingBot {
             String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
             if (message_text.equals("/udword")) {
-                SendMessage message = new SendMessage().setChatId(chat_id).setText(lastWordFromUrbanDictionaryDescription()).enableHtml(true);
+                SendMessage message = new SendMessage().setChatId(chat_id).setText(getRandomWordFromUrbanDictionary()).enableHtml(true);
                 try {
                     execute(message);
                 } catch (TelegramApiException e) {
@@ -38,44 +37,59 @@ public class ShoppingListBot extends TelegramLongPollingBot {
         return "ShoppingListBot";
     }
 
-    public String lastWordFromUrbanDictionaryDescription() {
+    public String getRandomWordFromUrbanDictionary() {
         StringBuilder builder = new StringBuilder();
         Document doc = null;
-        String dateString;
-        String url = "https://www.urbandictionary.com/?page=1";
+        Random random = new Random();
+        String url = "https://www.urbandictionary.com/random.php?page=" + (random.nextInt(999) + 1);
         try {
             doc = Jsoup.connect(url).get();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        System.out.println(url);
         if (doc != null) {
             Elements defPanels = doc.getElementsByClass("def-panel");
             Iterator<Element> defPanelsIterator = defPanels.iterator();
             if (defPanelsIterator.hasNext()) {
-                Element defPanel = defPanelsIterator.next();
-                Elements dates = defPanel.getElementsByClass("ribbon");
-                Iterator<Element> datesIterator = dates.iterator();
-                if (datesIterator.hasNext()) {
-                    dateString = datesIterator.next().text();
-                    builder.append("<i>").append(dateString).append("</i>\n\n");
-                }
+                Element defPanel = defPanels.get(random.nextInt(defPanels.size() - 1));
                 Elements words = defPanel.getElementsByClass("word");
                 Iterator<Element> wordsIterator = words.iterator();
                 if (wordsIterator.hasNext()) {
                     Element word = wordsIterator.next();
-                    builder.append("<b>").append(word.text()).append("</b>\n\n");
+                    builder.append("Term:\n").append("<b>").append(word.text()).append("</b>\n\n");
                 }
                 Elements meanings = defPanel.getElementsByClass("meaning");
                 Iterator<Element> meaningsIterator = meanings.iterator();
                 if (meaningsIterator.hasNext()) {
                     Element meaning = meaningsIterator.next();
-                    String meaningString = meaning.html().replaceAll("<br>", "\n");
-                    meaningString = meaningString.replaceAll("\n\n", "\n");
-                    builder.append(meaningString);
+                    String meaningString = getHrefsAndText(meaning);
+                    builder.append("Meaning:\n").append(meaningString).append("\n\n");
+                }
+                Elements examples = defPanel.getElementsByClass("example");
+                Iterator<Element> examplesIterator = examples.iterator();
+                if (examplesIterator.hasNext()) {
+                    Element example = examplesIterator.next();
+                    String exampleString = getHrefsAndText(example);
+                    builder.append("Examples:\n").append(exampleString);
                 }
             }
         }
         return builder.toString();
+    }
+
+    public static String getHrefsAndText(Element source) {
+        Pattern hrefPatter = Pattern.compile("href=\"[^\"]*\"");
+        String[] stringArray = source.html().split("</a>");
+        StringBuilder builder = new StringBuilder();
+        for (String string : stringArray) {
+            Matcher matcher = hrefPatter.matcher(string);
+            if (matcher.find()) {
+                String href = matcher.group(0);
+                string = string.replaceAll("<a.*>", "<a " + href + ">");
+                builder.append(string).append("</a>");
+            } else builder.append(string);
+        }
+        return builder.toString().replaceAll("\n", "").replaceAll("<br>", "\n");
     }
 }
